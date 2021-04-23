a110b0360240696aea7178725a60608f122c04cc
Tanaka Gundani <tanakagundani2@gmail.com>
2021-04-23 22:58:44 +0100

    Game

---
+++ Rock.pyc.py
@@ -0,0 +1,24 @@
+import random
+
+user = input("Enter a choice (rock, paper, scissors): ")
+choices = ["rock", "paper", "scissors"]
+computer_action = random.choice(choices)
+print(f"\nYou chose {user}, computer chose {computer_action}.\n")
+
+if user == computer_action:
+    print(f"Both players selected {user}. Draw!")
+elif user == "rock":
+    if computer_action == "scissors":
+        print("Winner!")
+    else:
+        print("loser")
+elif user == "paper":
+    if computer_action == "rock":
+        print("Winner!")
+    else:
+        print("loser")
+elif user == "scissors":
+    if computer_action == "paper":
+        print("Winner!")
+    else:
+        print("loser")
